e2a3872166a44df2d368637ec23e4ce350667a0f
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2011-11-02 18:38:08 -0600

    Added triple results.

--- Python/multiple.py
+++ Python/multiple.py
@@ -314,6 +314,7 @@ def Produce_Double():
     print "Coeffs_first=",coeffs_first
     print "Coeffs_second=",coeffs_second
     file_names=["steady_double_overall.eps","steady_double_first.eps","steady_double_second.eps"]        
+    
     for i in range(1,4):    
         pylab.figure(i)        
         legs=[r'''$U_{\mathrm{bubble}}='''+str(vel)[:4]+r'''$''' for vel in bubble_velocities]
@@ -328,7 +329,135 @@ def Produce_Double():
     #print "Aver_coefficient=",aver_coefficient
 
 
+def Produce_Triple():
+
+    #physical parameters
+    gamma=0.0728
+    diam=1.5e-3    
+    print os.getcwd()
+ 
+    styles=['kv-','ks-','ko-','k^-','k>-','k<-']
+    aver_coefficient=[]
+    bubble_velocities=[]
+    interface_velocities=[]
+    liquid_velocities=[]
+    gas_velocities=[]
+    holdups=[]    
+    #dirs=[3,5,8,10,20,40]
+    dirs=[3,5,8]    
+ 
+ 
+    coeffs_overall=[]
+    coeffs_first=[]
+    coeffs_second=[]
+    coeffs_third=[]
+    for counter,dir_name in enumerate(dirs):    
+        data_dir_name="SailfishData/"+str(dir_name)+"/"
+        file_name=data_dir_name+"capillary200000.npz"        
+        arr=numpy.load(file_name)        
+        phase=arr['phi']
+        velx=arr['v'][0]
+        vely=arr['v'][1]
+        dims=phase.shape
+
+        print "Dimensions=",dims
+
+        center=phase[dims[0]/2,:]
+        z1 = numpy.min(numpy.where(center < 0.0))
+        z2 = numpy.max(numpy.where(center < 0.0))
+        slug_length=dims[1]-z2+z1    
+        if z1==0:
+            z2=numpy.min(numpy.where(center>0.0))+dims[1]
+            z1=numpy.max(numpy.where(center>0.0))
+            slug_length=z1-z2%dims[1]
+        print z1,z2
+
+        interface_velocity=velx[dims[0]/2,z2%dims[1]]
+        capillary=interface_velocity*(2.0/3.0)/math.sqrt(8*0.04*0.04/9.0)
+        reynolds=interface_velocity*dims[0]/(2.0/3.0)        
+        phys_velocity=math.sqrt(capillary*reynolds*gamma/(1000*diam))        
+        superficial_liquid=numpy.sum(velx[1:-1,((z1+z2+dims[1])/2)%dims[1]])/(dims[0]-2)
+        gas_holdup=float(len(numpy.where(phase<0)[0]))/(dims[1]*(dims[0]-2))
+        
+        interface_velocities.append(interface_velocity)
+        bubble_velocities.append(phys_velocity)
+        liquid_velocities.append(superficial_liquid)
+        gas_velocities.append(gas_holdup*interface_velocity)        
+                
+        holdups.append(gas_holdup)
+        
+        print "Interface velocity=",interface_velocity
+        print "Capillary=",capillary
+        print "Reynolds=",reynolds
+  
+        results_dir_name="SailfishMassCentralTriple/"+str(dir_name)+"/"     
+        
+        vx=numpy.loadtxt("SailfishResultsCenter/"+str(dir_name)+"/ux.dat").transpose()
+        coefficient_overall=[]
+        coefficient_first=[]
+        coefficient_second=[]
+        coefficient_third=[]
+        for file_number in numpy.arange(1160000,4000000,280000):
+            density=numpy.loadtxt(results_dir_name+"density_jos_par"+str(file_number)+".dat")
+            inlet_conc=numpy.sum(density[1:-1,1]*vx[1:-1,1])/numpy.sum(vx[1:-1,1])
+            outlet_conc=numpy.sum(density[1:-1,-2]*vx[1:-1,-2])/numpy.sum(vx[1:-1,-2])
+            middle_1_conc=numpy.sum(density[1:-1,density.shape[1]/3]*vx[1:-1,-2])/numpy.sum(vx[1:-1,-2])
+            middle_2_conc=numpy.sum(density[1:-1,2*density.shape[1]/3]*vx[1:-1,-2])/numpy.sum(vx[1:-1,-2])           
+            print "Middle=",middle_1_conc,middle_2_conc
+            
+            coefficient_overall.append((superficial_liquid+gas_holdup*interface_velocity)*phys_velocity/interface_velocity\
+                   /(3*15*diam)*numpy.log((1-numpy.abs(inlet_conc))/(1-numpy.abs(outlet_conc))))
+            coefficient_first.append((superficial_liquid+gas_holdup*interface_velocity)*phys_velocity/interface_velocity\
+                   /(15*diam)*numpy.log((1-numpy.abs(inlet_conc))/(1-numpy.abs(middle_1_conc))))
+            coefficient_second.append((superficial_liquid+gas_holdup*interface_velocity)*phys_velocity/interface_velocity\
+                   /(15*diam)*numpy.log((1-numpy.abs(middle_1_conc))/(1-numpy.abs(middle_2_conc))))
+            coefficient_third.append((superficial_liquid+gas_holdup*interface_velocity)*phys_velocity/interface_velocity\
+                   /(15*diam)*numpy.log((1-numpy.abs(middle_2_conc))/(1-numpy.abs(outlet_conc))))
+            
+            
+        coefficient_overall=numpy.array(coefficient_overall)
+        coefficient_first=numpy.array(coefficient_first)
+        coefficient_second=numpy.array(coefficient_second)
+        coefficient_third=numpy.array(coefficient_third)
+        print "Coefficient_first=",coefficient_first
+        print "Coefficient_second=",coefficient_second
+        print "Coefficient_third=",coefficient_third
+        time_iterations=numpy.arange(1160000,4000000,280000)
+        inds=numpy.where(time_iterations>3300000)        
+        print coefficient_overall[inds[0]]        
+        coeffs_overall.append(numpy.mean(coefficient_overall[inds[0]]))
+        coeffs_first.append(numpy.mean(coefficient_first[inds[0]]))
+        coeffs_second.append(numpy.mean(coefficient_second[inds[0]]))
+        coeffs_third.append(numpy.mean(coefficient_third[inds[0]]))
+        #numpy.savetxt(results_dir_name+"coefficient.dat",zip(time_iterations,coefficient))           
+        
+        pylab.figure(1)
+        pylab.plot(time_iterations,coefficient_overall,styles[counter])        
+        pylab.figure(2)
+        pylab.plot(time_iterations,coefficient_first,styles[counter])        
+        pylab.figure(3)
+        pylab.plot(time_iterations,coefficient_second,styles[counter])
+        pylab.figure(4)
+        pylab.plot(time_iterations,coefficient_third,styles[counter])
+        
+        #aver_coefficient.append(numpy.mean(coefficient[len(coefficient)/2:]))
+    print "Coeffs_overall=", coeffs_overall
+    print "Coeffs_first=",coeffs_first
+    print "Coeffs_second=",coeffs_second
+    print "Coeffs_third=",coeffs_third
+    file_names=["steady_triple_overall.eps","steady_triple_first.eps","steady_triple_second.eps","steady_triple_third.eps"]        
+    
+    for i in range(1,5):    
+        pylab.figure(i)        
+        legs=[r'''$U_{\mathrm{bubble}}='''+str(vel)[:4]+r'''$''' for vel in bubble_velocities]
+        pylab.xlabel('Iterations',fontsize=20)
+        pylab.ylabel(r'''$k_L a, \mathrm{s^{-1}}$''',fontsize=20)
+        pylab.legend(legs,fancybox=True,labelspacing=0.1)
+        pylab.ylim(ymax=9)
+        pylab.savefig(file_names[i-1],dpi=300)
+
+
 
 if __name__=="__main__":
-    Produce_Double()    
+    Produce_Triple()    
     pylab.show()
