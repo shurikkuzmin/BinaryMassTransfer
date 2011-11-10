f11c06d96e0e4843e2137b26b4b1624894050d23
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2011-11-09 17:05:26 -0700

    Added cylinder benchmark.

--- Benchmarks/cylinder.cpp
+++ Benchmarks/cylinder.cpp
@@ -12,13 +12,14 @@
 int NY;
 int NX;
 int NUM;
+int radius;
 
 //Other constants
 const int NPOP=9;
 
 //Time steps
-int N=200000;
-int NOUTPUT=1000;
+int N=64000;
+int NOUTPUT=1600;
 
 //Fields and populations
 double *f;
@@ -36,7 +37,7 @@ int *bottom;
 int *top;
 
 //BGK relaxation parameter
-double omega=1.0/1.99;
+double omega=1.0/(0.5+0.5/32.0); ///1.99;
 double omega_plus=2.0-omega;
 double omega_minus=omega;
 
@@ -70,6 +71,24 @@ void writedensity(std::string const & fname)
 		fout<<"\n";
 	}
 }
+
+void writegeometry(std::string const & fname)
+{
+	std::string filename=fname+".dat";
+	std::ofstream fout(filename.c_str());
+	fout.precision(10);
+
+	for (int iX=0; iX<NX; ++iX)
+	{
+		for (int iY=0; iY<NY; iY++)
+		{
+			int counter=iY*NX+iX;
+			fout<<geometry[counter]<<" ";
+		}
+		fout<<"\n";
+	}
+}
+
 
 
 void init()
@@ -138,111 +157,86 @@ void collide_column_bgk(int coor_y,int coor_bottom,int coor_top)
 
 
 
-void collide_column(int coor_y,int coor_bottom,int coor_top)
+
+void collide()
 {
-	for(int iX=coor_bottom;iX<=coor_top;iX++)
-	{
-		int counter=coor_y*NX+iX;
-		rho[counter]=0.0;
-        int offset=counter*NPOP;
- 
-		double sum=0;
-		for(int k=0;k<NPOP;k++)
-			sum+=f[offset+k];
+	for (int counter=0;counter<NUM;counter++)
+		if (geometry[counter]==1)
+		{
+			rho[counter]=0.0;
+		    
+		    int offset=counter*NPOP;
+	 
+			double sum=0;
+			for(int k=0;k<NPOP;k++)
+				sum+=f[offset+k];
 	
-		rho[counter]=sum;
+			rho[counter]=sum;
 
-		double dense_temp=rho[counter];
-		double ux_temp=ux[counter];
-		double uy_temp=uy[counter];
+			double dense_temp=rho[counter];
+			double ux_temp=ux[counter];
+			double uy_temp=uy[counter];
 
-		//BGK equilibrium
-		double feq[NPOP];
+			//BGK equilibrium
+			double feq[NPOP];
 	
-		//TRT equilibrium
-		double feq_plus[NPOP],feq_minus[NPOP];
-        double f_plus[NPOP],f_minus[NPOP];
+			//TRT equilibrium
+			double feq_plus[NPOP],feq_minus[NPOP];
+		    double f_plus[NPOP],f_minus[NPOP];
 
-         //Speeding up the process
-        f_plus[0]=f[offset];
-        f_plus[1]=0.5*(f[offset+1]+f[offset+3]);
-        f_plus[2]=0.5*(f[offset+2]+f[offset+4]);
-        f_plus[3]=f_plus[1];
-        f_plus[4]=f_plus[2];
-        f_plus[5]=0.5*(f[offset+5]+f[offset+7]);
-        f_plus[6]=0.5*(f[offset+6]+f[offset+8]);
-        f_plus[7]=f_plus[5];
-        f_plus[8]=f_plus[6];
+		     //Speeding up the process
+		    f_plus[0]=f[offset];
+		    f_plus[1]=0.5*(f[offset+1]+f[offset+3]);
+		    f_plus[2]=0.5*(f[offset+2]+f[offset+4]);
+		    f_plus[3]=f_plus[1];
+		    f_plus[4]=f_plus[2];
+		    f_plus[5]=0.5*(f[offset+5]+f[offset+7]);
+		    f_plus[6]=0.5*(f[offset+6]+f[offset+8]);
+		    f_plus[7]=f_plus[5];
+		    f_plus[8]=f_plus[6];
 
-        f_minus[0]=0.0;
-        f_minus[1]=0.5*(f[offset+1]-f[offset+3]);
-        f_minus[2]=0.5*(f[offset+2]-f[offset+4]);
-        f_minus[3]=-f_minus[1];
-        f_minus[4]=-f_minus[2];
-        f_minus[5]=0.5*(f[offset+5]-f[offset+7]);
-        f_minus[6]=0.5*(f[offset+6]-f[offset+8]);
-        f_minus[7]=-f_minus[5];
-        f_minus[8]=-f_minus[6];
-      
-        //The equilibrium populations are taken from Irina's bb_pressure and bb_examples
-        feq_minus[0]=0.0;
-        feq_minus[1]=weights_trt[1]*dense_temp*(cx[1]*ux_temp+cy[1]*uy_temp);
-        feq_minus[2]=weights_trt[2]*dense_temp*(cx[2]*ux_temp+cy[2]*uy_temp);
-        feq_minus[3]=-feq_minus[1];
-        feq_minus[4]=-feq_minus[2];
-        feq_minus[5]=weights_trt[5]*dense_temp*(cx[5]*ux_temp+cy[5]*uy_temp);
-        feq_minus[6]=weights_trt[6]*dense_temp*(cx[6]*ux_temp+cy[6]*uy_temp);
-        feq_minus[7]=-feq_minus[5];
-        feq_minus[8]=-feq_minus[6];
-        
-        double u_sq=ux_temp*ux_temp+uy_temp*uy_temp;
+		    f_minus[0]=0.0;
+		    f_minus[1]=0.5*(f[offset+1]-f[offset+3]);
+		    f_minus[2]=0.5*(f[offset+2]-f[offset+4]);
+		    f_minus[3]=-f_minus[1];
+		    f_minus[4]=-f_minus[2];
+		    f_minus[5]=0.5*(f[offset+5]-f[offset+7]);
+		    f_minus[6]=0.5*(f[offset+6]-f[offset+8]);
+		    f_minus[7]=-f_minus[5];
+		    f_minus[8]=-f_minus[6];
+		  
+		    //The equilibrium populations are taken from Irina's bb_pressure and bb_examples
+		    feq_minus[0]=0.0;
+		    feq_minus[1]=weights_trt[1]*dense_temp*(cx[1]*ux_temp+cy[1]*uy_temp);
+		    feq_minus[2]=weights_trt[2]*dense_temp*(cx[2]*ux_temp+cy[2]*uy_temp);
+		    feq_minus[3]=-feq_minus[1];
+		    feq_minus[4]=-feq_minus[2];
+		    feq_minus[5]=weights_trt[5]*dense_temp*(cx[5]*ux_temp+cy[5]*uy_temp);
+		    feq_minus[6]=weights_trt[6]*dense_temp*(cx[6]*ux_temp+cy[6]*uy_temp);
+		    feq_minus[7]=-feq_minus[5];
+		    feq_minus[8]=-feq_minus[6];
+		    
+		    double u_sq=ux_temp*ux_temp+uy_temp*uy_temp;
 		
-		//Check with Irina - need to correct t_q^{(u)}\neq t_q^{(a)}        
-        feq_plus[1]=weights_trt[1]*dense_temp*(ce+0.5*(3.0*(cx[1]*ux_temp+cy[1]*uy_temp)*(cx[1]*ux_temp+cy[1]*uy_temp)-u_sq));
-        feq_plus[2]=weights_trt[2]*dense_temp*(ce+0.5*(3.0*(cx[2]*ux_temp+cy[2]*uy_temp)*(cx[2]*ux_temp+cy[2]*uy_temp)-u_sq));
-        feq_plus[3]=feq_plus[1];
-        feq_plus[4]=feq_plus[2];
-        feq_plus[5]=weights_trt[5]*dense_temp*(ce+0.5*(3.0*(cx[5]*ux_temp+cy[5]*uy_temp)*(cx[5]*ux_temp+cy[5]*uy_temp)-u_sq));
-        feq_plus[6]=weights_trt[6]*dense_temp*(ce+0.5*(3.0*(cx[6]*ux_temp+cy[6]*uy_temp)*(cx[6]*ux_temp+cy[6]*uy_temp)-u_sq));
-        feq_plus[7]=feq_plus[5];
-        feq_plus[8]=feq_plus[6];
-        feq_plus[0]=dense_temp-2.0*(feq_plus[1]+feq_plus[2]+feq_plus[5]+feq_plus[6]);
+			//Check with Irina - need to correct t_q^{(u)}\neq t_q^{(a)}        
+		    feq_plus[1]=weights_trt[1]*dense_temp*(ce+0.5*(3.0*(cx[1]*ux_temp+cy[1]*uy_temp)*(cx[1]*ux_temp+cy[1]*uy_temp)-u_sq));
+		    feq_plus[2]=weights_trt[2]*dense_temp*(ce+0.5*(3.0*(cx[2]*ux_temp+cy[2]*uy_temp)*(cx[2]*ux_temp+cy[2]*uy_temp)-u_sq));
+		    feq_plus[3]=feq_plus[1];
+		    feq_plus[4]=feq_plus[2];
+		    feq_plus[5]=weights_trt[5]*dense_temp*(ce+0.5*(3.0*(cx[5]*ux_temp+cy[5]*uy_temp)*(cx[5]*ux_temp+cy[5]*uy_temp)-u_sq));
+		    feq_plus[6]=weights_trt[6]*dense_temp*(ce+0.5*(3.0*(cx[6]*ux_temp+cy[6]*uy_temp)*(cx[6]*ux_temp+cy[6]*uy_temp)-u_sq));
+		    feq_plus[7]=feq_plus[5];
+		    feq_plus[8]=feq_plus[6];
+		    feq_plus[0]=dense_temp-2.0*(feq_plus[1]+feq_plus[2]+feq_plus[5]+feq_plus[6]);
 
- 
-		//Collision operator
-		for(int k=0; k < NPOP; k++)
-			f2[offset+k]=f[offset+k]-omega_plus*(f_plus[k]-feq_plus[k])-omega_minus*(f_minus[k]-feq_minus[k]);
-
+	 
+			//Collision operator
+			for(int k=0; k < NPOP; k++)
+				f2[offset+k]=f[offset+k]-omega_plus*(f_plus[k]-feq_plus[k])-omega_minus*(f_minus[k]-feq_minus[k]);
 	}
-
-	
-}
-
-void collide_bulk()
-{
-
-    for(int iY=1;iY<NY-1;iY++)
-		collide_column(iY,bottom[iY],top[iY]);
 			
 }
 
-void update_inlet_outlet()
-{
-	for(int iX=1;iX<NX-1;iX++)
-	{
-		int counter_inlet=iX;
-		int counter_outlet=(NY-1)*NX+iX;
-		for(int k=0;k<NPOP;k++)
-		{
-			rho[counter_inlet]=rho[counter_inlet+NX];
-			rho[counter_outlet]=rho[counter_outlet-NX];
-			f2[counter_inlet*NPOP+k]=f2[(counter_inlet+NX)*NPOP+k];
-			f2[counter_outlet*NPOP+k]=f2[(counter_outlet-NX)*NPOP+k];
-		}   	
-	
-	}
-
-}
 
 void update_bounce_back()
 {
@@ -252,79 +246,82 @@ void update_bounce_back()
 		{
 			int dir=dirs[counter][k];
 			int counter2=bb_nodes[counter]+cy[dir]*NX+cx[dir];
-			f2[bb_nodes[counter]*NPOP+dir]=-f2[counter2*NPOP+compliment[dir]]+2*weights[dir]*conc_bubble;
+			f2[bb_nodes[counter]*NPOP+dir]=-f2[counter2*NPOP+compliment[dir]]+2*weights[dir]*conc_wall;
 		}
-		
 	}
-	
-	//BB nodes density and velocity specification
-	for(int iY=0;iY<NY;iY++)
-	{
-		int counter=iY*NX;
-		int counter_top=((iY+1+NY)%NY)*NX;
-		int counter_bottom=((iY-1+NY)%NY)*NX;
 
-
-		f2[counter*NPOP+1]=f2[(counter+1)*NPOP+3];
-		f2[counter*NPOP+5]=f2[(counter_top+1)*NPOP+7];
-		f2[counter*NPOP+8]=f2[(counter_bottom+1)*NPOP+6];
-
-		f2[(counter+NX-1)*NPOP+3]=f2[(counter+NX-2)*NPOP+1];
-		f2[(counter+NX-1)*NPOP+6]=f2[(counter_top+NX-2)*NPOP+8];
-		f2[(counter+NX-1)*NPOP+7]=f2[(counter_bottom+NX-2)*NPOP+5];
-
-		rho[counter]=conc_wall;
-		rho[counter+NX-1]=conc_wall;
-		ux[counter]=0.0;
-		ux[counter+NX-1]=0.0;
-		uy[counter]=0.0;
-		uy[counter+NX-1]=0.0;
-	}
 
 }
 
 void initialize_geometry()
 {
-	NY=3001;
-	NX=202;
+	NY=129;
+	NX=129;
 	NUM=NX*NY;
     geometry=new int[NUM];
     rho=new double[NUM];
     ux=new double[NUM];
     uy=new double[NUM];
-    bottom=new int[NY];
-    bottom_mid=new int[NY];
-    top=new int[NY];
-    top_mid=new int[NY];
+    radius=40;
     
-	std::ifstream fin("geometry.dat");
-	std::ifstream fux("ux.dat");
-	std::ifstream fuy("uy.dat");
-	//std::ifstream fux("vely0080000.dat");
-	//std::ifstream fuy("velx0080000.dat");
-	
-	if ((fux==NULL) || (fuy==NULL))
-	{
-		std::cout<<"Get the fuck right name of the filename\n";
-		exit(0);
-	}
-	
-	//Reading files
-	for(int counter=0;counter<NUM;counter++)
-	{
-		fin>>geometry[counter];
-		fux>>uy[counter];
-		fuy>>ux[counter];
-		uy[counter]=coefficient*uy[counter];
-		ux[counter]=coefficient*ux[counter];
-	}
+    for(int iX=0;iX<NX;iX++)
+    	for(int iY=0;iY<NY;iY++)
+    	{
+    		int counter=iX*NY+iY;
+    		if ((iX-(NX-1)/2)*(iX-(NX-1)/2)+(iY-(NY-1)/2)*(iY-(NY-1)/2)>radius*radius)
+    	    	geometry[counter]=-1;
+    	    else 
+    	    	geometry[counter]=1;
+    	}
+    writegeometry("geometry_before");    
+
+	for(int iX=0;iX<NX;iX++)
+	    for(int iY=0;iY<NY;iY++)
+	    {
+	        int counter=iX*NY+iY;
+	        if (geometry[counter]==1)
+	        {	
+	        	bool flag=false;
+	        	for(int k=0;k<NPOP;k++)
+	        	{
+	        		int counter2=(iX+cx[k])*NY+iY+cy[k];
+	        		if (geometry[counter2]==-1)
+	        			flag=true;
+	        	}
+	        	if (flag)
+	         		geometry[counter]=0;
+	        }
+	        
+	    }
+
+	for(int iX=0;iX<NX;iX++)
+	    for(int iY=0;iY<NY;iY++)
+	    {
+	        int counter=iX*NY+iY;
+	        if (geometry[counter]==0)
+	        {	
+	        	bool flag=false;
+	        	for(int k=0;k<NPOP;k++)
+	        	{
+	        		int counter2=(iX+cx[k])*NY+iY+cy[k];
+	        		if (geometry[counter2]==1)
+	        			flag=true;
+	        	}
+	        	if (!flag)
+	         		geometry[counter]=-1;
+	        }
+	        
+	    }
+	    
+	    
+	writegeometry("geometry_after");
 
 	//Initialization
     for(int counter=0;counter<NUM;counter++)
     {
 		if (geometry[counter]==0)
 		{
-		    rho[counter]=conc_bubble;
+		    rho[counter]=conc_wall;
 			ux[counter]=0.0;
 			uy[counter]=0.0;
 			bb_nodes.push_back(counter);
@@ -336,78 +333,13 @@ void initialize_geometry()
 			uy[counter]=0.0;
 		}
 		else
+		{
 			rho[counter]=0.0;
-	}
-	
-	//Finding bulk nodes
-	int iX=0;
-	int iY=0;
-	while(iY<NY)
-	{
-	    bool flag=false;
-	    iX=0;
-	    while(iX<NX)
-	    {
-			int counter=iY*NX+iX;
-		    if (geometry[counter]==0) 
-		    {
-				flag=true;
-				bottom[iY]=iX-1;
-				while((geometry[counter]==0) || (geometry[counter]==-1))
-				{
-					iX++;
-					counter=iY*NX+iX;
-				}
-				if (iX<=NX/2)
-				{
-					bottom_mid[iY]=iX;
-					while(geometry[counter]==1)
-					{
-						iX++;
-						counter=iY*NX+iX;
-					}
-					top_mid[iY]=iX-1;
-					while((geometry[counter]==0) || (geometry[counter]==-1))
-					{
-						iX++;
-						counter=iY*NX+iX;
-					}
-				    top[iY]=iX;
-	
-				}
-				else
-				{
-					top[iY]=iX;
-					bottom_mid[iY]=top_mid[iY]=0;
-				}
-			}
-			iX++;
+			ux[counter]=0.0;
+			uy[counter]=0.0;
 		}
-		if (!flag)
-			bottom[iY]=bottom_mid[iY]=top[iY]=top_mid[iY]=0;
-		iY++;
 	}
 	
-	std::ofstream fcoor("coor.dat");
-	for(int iY=0;iY<NY;iY++)
-	{
-		fcoor<<bottom[iY]<<" ";
-	}
-	fcoor<<"\n";
-	for(int iY=0;iY<NY;iY++)
-	{
-		fcoor<<top[iY]<<" ";
-	}
-	fcoor<<"\n";
-	for(int iY=0;iY<NY;iY++)
-	{
-		fcoor<<bottom_mid[iY]<<" ";
-	}
-	fcoor<<"\n";
-	for(int iY=0;iY<NY;iY++)
-	{
-		fcoor<<top_mid[iY]<<" ";
-	}
 	
 	//Finding directions for BB nodes
     dirs=new std::vector<char>[bb_nodes.size()];
@@ -431,112 +363,35 @@ void finish_simulation()
 	delete[] uy;
 	delete[] f;
 	delete[] f2;
-	delete[] bottom;
-	delete[] bottom_mid;
-	delete[] top_mid;
-	delete[] top;
 	delete[] dirs;
 }
 
-void stream_column(int coor_y,int coor_bottom,int coor_top)
-{
-
-	for(int iX=coor_bottom;iX<=coor_top;iX++)
-	{
-		int counter=coor_y*NX+iX;
-		for(int iPop=0;iPop<NPOP;iPop++)
-		{
-			int iX2=(iX-cx[iPop]+NX)%NX;
-			int iY2=(coor_y-cy[iPop]+NY)%NY;
-						
-			int counter2=iY2*NX+iX2;
-			f[counter*NPOP+iPop]=f2[counter2*NPOP+iPop];
-		}
-	}
-}
 
 void stream()
 {
-	
-    for(int iY=1;iY<NY-1;iY++)
-		if (bottom[iY]==top[iY])
-			stream_column(iY,1,NX-2);
-		else
-		{
-			if(top_mid[iY]==bottom_mid[iY])
+	for(int counter=0;counter<=NUM;counter++)
+		if (geometry[counter]==1)
+			for(int iPop=0;iPop<NPOP;iPop++)
 			{
-				stream_column(iY,1,bottom[iY]);
-				stream_column(iY,top[iY],NX-2);
+				int counter2=counter-cy[iPop]*NX-cx[iPop];
+				f[counter*NPOP+iPop]=f2[counter2*NPOP+iPop];
 			}
-			else
-			{
-				stream_column(iY,1,bottom[iY]);
-				stream_column(iY,bottom_mid[iY],top_mid[iY]);
-				stream_column(iY,top[iY],NX-2);
-			}
-		}
 
-	
 }
 
-void calculate_mass_transfer(int time_counter)
-{
-	static std::ofstream fconc("concentration.dat");
-	
-    double conc=0.0;
-    int counter; 	
-    for(int iY=0;iY<NY;iY++)
-		if (bottom[iY]==top[iY])
-			for(int iX=1;iX<NX-1;iX++)
-				conc+=rho[iY*NX+iX];
-		else
-		{
-			if (top_mid[iY]==bottom_mid[iY])
-			{
-				for(int iX=1;iX<=bottom[iY];iX++)
-					conc+=rho[iY*NX+iX];
-				for(int iX=top[iY];iX<NX-1;iX++)
-					conc+=rho[iY*NX+iX];
-			}
-			else
-			{
-				for(int iX=1;iX<=bottom[iY];iX++)
-					conc+=rho[iY*NX+iX];
-				for(int iX=bottom_mid[iY];iX<=top_mid[iY];iX++)
-					conc+=rho[iY*NX+iX];
-				for(int iX=top[iY];iX<NX-1;iX++)
-					conc+=rho[iY*NX+iX];
-			}
-		}
-		
-	double conc_inlet=0.0;
-	double conc_outlet=0.0;
-	double sum_vel_inlet=0.0;
-	double sum_vel_outlet=0.0;
-	for(int iX=1;iX<NX-1;iX++)
-	{
-		conc_inlet+=rho[iX]*uy[iX];
-		sum_vel_inlet+=uy[iX];
-		conc_outlet+=rho[(NY-1)*NX+iX]*uy[(NY-1)*NX+iX];
-		sum_vel_outlet+=uy[(NY-1)*NX+iX];
-	}
-	
-
-	fconc<<time_counter<<" "<<conc<<" "<<conc_inlet/sum_vel_inlet<<" "<<conc_outlet/sum_vel_outlet<<"\n"<<std::flush;
-}
 
 int main(int argc, char* argv[])
 {
 
+
     initialize_geometry();
     init();
-      
+    
 
 	for(int counter=0;counter<=N;counter++)
 	{
 
-        collide_bulk();
-        update_inlet_outlet();
+        collide();
         update_bounce_back();
 		stream();
         
@@ -553,7 +408,6 @@ int main(int argc, char* argv[])
 			filewritedensity<<"density"<<std::string(7-counterconvert.str().size(),'0')<<counter;
 			
  			writedensity(filewritedensity.str());
-		    calculate_mass_transfer(counter);
 		}
 
 	}
---
+++ Python/benchmarks.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import numpy
+import pylab
+import scipy.special
+
+coeff=32
+omega=1.0/(0.5+0.5/coeff)
+radius=39
+diffusion=1.0/3.0*(1.0/omega-0.5)
+num_terms=20
+
+def read_file(file_name):
+    concentration=numpy.loadtxt(file_name)
+    pylab.imshow(concentration)
+    
+
+def read_files(file_dir):
+    styles=["kv","k^","ks","ko","kD"]    
+    non_time=[]    
+    legs=[]    
+    for num,counter in enumerate(range(200*coeff,2000*coeff,400*coeff)):
+        file_name=file_dir+"density"+(7-len(str(counter)))*"0"+str(counter)+".dat"
+        concentration=numpy.loadtxt(file_name)
+        dims=concentration.shape
+        #pylab.figure()
+        #pylab.imshow(concentration)
+        x=numpy.arange(0,dims[1])        
+        x_rad=numpy.linspace(0.0,1.0,radius+1)
+
+        
+        bessel=numpy.zeros_like(x)
+        bessel_zeros=scipy.special.jn_zeros(0,num_terms)
+        non_time.append(diffusion*counter/(radius*radius))
+        for term in range(0,num_terms):
+            bessel=bessel+2/(bessel_zeros[term]*scipy.special.jn(1,bessel_zeros[term]))\
+                *numpy.exp(-bessel_zeros[term]*bessel_zeros[term]*diffusion*counter/(radius*radius))\
+                *scipy.special.jn(0,bessel_zeros[term]*x/radius)
+        bessel=1-bessel
+        #bessel[numpy.where(bessel)>1]=0
+        #bessel=numpy.roll(bessel,dims[1]/2)
+        print "Shape",dims        
+        pylab.figure(1)
+        pylab.plot(x_rad,concentration[(dims[0]-1)/2,(dims[1]-1)/2:(dims[1]-1)/2+radius+1],styles[num])        
+        #pylab.plot(concentration[dims[0]/2-1,dims[1]/2:dims[1]/2+radius])
+        #pylab.figure(2)
+        pylab.plot(x_rad,bessel[numpy.where(x<=radius)],styles[num]+"--",markerfacecolor="None")
+    legs=[[r'''$\tau='''+str(time)[0:6]+'''$''',r'''$\tau='''+str(time)[0:6]+'''$'''] for time in non_time]    
+    legs=numpy.ravel(legs)   
+    pylab.title(r'''$D='''+str(diffusion)[0:6]+'''$''',fontsize=30)
+    pylab.legend(legs,fancybox=True,loc=2)
+    pylab.xlabel(r'''$x$''',fontsize=20)
+    pylab.ylabel(r'''$C$''',fontsize=20)
+    #pylab.legend(legs,fancybox=True,labelspacing=0.1)
+    pylab.savefig("cylinder"+str(diffusion)[2:6]+".eps",dpi=300)        
+        
+def show_bessel():
+    x=numpy.arange(0.0,10.0,0.1)
+    bessel=scipy.special.jn(0,x)
+    pylab.plot(x,bessel)
+if __name__=="__main__":
+    file_name="../Benchmarks/density0001000.dat"
+    file_dir="../Benchmarks/"
+    #read_file(file_name)
+    
+    read_files(file_dir)
+    #show_bessel()    
+    pylab.show()
